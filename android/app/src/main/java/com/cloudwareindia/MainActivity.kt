65c66c5b15c4ef9aa4f46259495fa40ee1d48cec
Manish Kumar Mourya <manish.mourya@theninehertzindia.com>
2024-10-16 18:47:49 +0530

    Updating pipline for dev_chat branch

--- android/app/src/main/java/com/cloudwareindia/MainActivity.kt
+++ android/app/src/main/java/com/cloudwareindia/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.android
+package com.cloudwareindia
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/cloudwareindia/MainApplication.kt
+++ android/app/src/main/java/com/cloudwareindia/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.android
+package com.cloudwareindia
 
 import android.app.Application
 import com.facebook.react.PackageList
